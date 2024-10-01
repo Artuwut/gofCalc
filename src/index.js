740e78cba10231a14d57e2aea2d0ee63899cb5ae
Artuwut <krtur58@gmx.de>
2024-10-01 16:33:33 +0200

    Local changes to index.js

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ if (process.env.NODE_ENV !== 'production') {
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
